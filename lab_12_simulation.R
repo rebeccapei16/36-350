ac9fba18149f95ea45435960c27ebae0fe0807d0
Rebecca Pei <rebeccapei@MacBook-Pro-2.local>
2019-08-01 04:39:50 +0800

    add run simulation, add graphs

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -37,3 +37,45 @@ model_select <- function(covariates, responses, cutoff) {
   
   return(new_p_value)
 }
+
+
+library(ggplot2)
+library(gridExtra)
+
+#' run_simulation
+#'
+#' @param n_trials number of trials
+#' @param n row of matrix
+#' @param p col of matrix
+#' @param cutoff p value < cutoff, then variable significant
+#'
+#' @return all the significant p-values
+#' @export
+#'
+#' @examples
+run_simulation <- function(n_trials, n, p, cutoff) {
+  library(ggplot2)
+  p_values <- c()
+  for(i in (1:n_trials)){
+    lst <- generate_data(n, p)
+    current_p_value <- model_select(lst$covariates, lst$responses, cutoff)
+    p_values <- c(p_values, current_p_value)
+  }
+  gg <- ggplot(data.frame(x = p_values), aes(x = x)) +
+    geom_histogram(bins = 10) +
+    labs(title = paste("n = ", n, ", p = ", p),
+         x = "p values")
+  return(list(p_values = p_values, graph = gg))
+}
+
+
+
+gg_p_value <- list()
+i <- 1
+for (n in c(100, 1000, 10000)){
+  for (p in c(10, 20, 50)) {
+    gg_p_value[[i]] <- run_simulation(1000, n, p, 0.05)$graph
+    i <- i + 1
+  }
+}
+grid.arrange(grobs = gg_p_value)
