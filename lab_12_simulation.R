766ea9d2ce5479decfff8c5913f9858744221774
Rebecca Pei <rebeccapei@MacBook-Pro-2.local>
2019-08-01 04:35:31 +0800

    add model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -11,3 +11,29 @@ generate_data <- function(n, p) {
   return(list(covariates = matrix(rnorm(n*p), ncol = p),
               responses = rnorm(n)))
 }
+
+
+#' model_select
+#'
+#' @param covariates a n by p matrix
+#' @param responses  vector of length n
+#' @param cutoff if p-value < cutoff, then significant
+#'
+#' @return p-values of the new model
+#' @export
+#'
+#' @examples
+model_select <- function(covariates, responses, cutoff) {
+  fit <- lm(responses ~ covariates)
+  p_value <- summary(fit)$coefficients[-1,"Pr(>|t|)"]
+  sig_var_index <- which(summary(fit)$coefficients[-1,"Pr(>|t|)"] < cutoff)
+  
+  if (all(sig_var_index == F)){
+    return(NULL)
+  }
+  
+  fit_new <- lm(responses ~ covariates[, sig_var_index])
+  new_p_value <- summary(fit_new)$coefficients[-1,"Pr(>|t|)"]
+  
+  return(new_p_value)
+}
